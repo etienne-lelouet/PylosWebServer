0c8058485f0aad0483abebb9e966c400e6ec3ad2
etienne-lelouet <etiennelelouet@outlook.com>
2019-01-25 05:28:26 +0100

    try again

--- src/controllers/playerInfo.js
+++ src/controllers/playerInfo.js
@@ -21,7 +21,7 @@ const playerInfoController = (req, res) => {
 		if (!req.decoded) {
 			return 0;
 		}
-		if (req.params.user && req.params.user.length > 3) {
+		if (req.params.user && req.params.user.length() > 3) {
 			req.decoded.login = req.params.user;
 		}
 		User.findOne(({ login: req.decoded.login }), (err, user) => {
